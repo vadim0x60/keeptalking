0f1e8e7195d169d741b5c76a508b1b72a3d5ae65
Vadim Liventsev <dev@vadim.me>
2025-08-24 23:28:06 +0200

    everything's beautiful and nothing works

---
+++ example.py
@@ -0,0 +1,38 @@
+import requests
+from keeptalking import vibe
+import asyncio
+import logging
+import openai
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
+
+@vibe()
+async def is_general_purpose(descr) -> bool:
+    """Filter out coding models and other non-general language models"""
+    return f"Is this a general purpose language model?\n\n{descr}"
+
+async def is_alive(slug):
+    @vibe(model=slug)
+    async def greet():
+        """Greeting test to check if the LLM is alive"""
+        return "Hi!"
+
+    try:
+        logging.info(await greet())
+        return True
+    except (openai.NotFoundError, openai.InternalServerError):
+        return False
+
+async def openrouter_models():
+    models = requests.get("https://openrouter.ai/api/frontend/models/find?order=top-weekly").json()['data']['models'][:3]
+    models = ((model, is_general_purpose(model['description'])) for model in models)
+    models = (model for model, general_purpose in models if await general_purpose)
+    models = ((model, is_alive(model['slug'])) async for model in models)
+    models = (model async for model, alive in models if await alive)
+
+    async for model in models:
+        print(model['slug'])
+
+if __name__ == '__main__':  
+    asyncio.run(openrouter_models())
---
+++ keeptalking.py
@@ -0,0 +1,81 @@
+import os
+from functools import wraps
+from openai import AsyncOpenAI, OpenAI
+from pydantic import create_model, BaseModel
+import asyncio
+
+DEFAULT_TOKENS = 2048
+DEFAULT_MODEL = 'openrouter/auto'
+
+class SyncAsyncError(RuntimeError):
+    pass
+
+params = {
+    'max_retries': int(os.getenv("MAX_RETRIES", 50))
+}
+base_url = os.getenv("BASE_URL")
+if base_url:
+    params['base_url'] = base_url
+    params['api_key'] = os.getenv("API_KEY")
+else:
+    api_key = os.getenv("OPENROUTER_API_KEY")
+    if api_key:
+        params['base_url'] = 'https://api.openrouter.ai/api/v1'
+        params['api_key'] = api_key
+    else:
+        api_key = os.getenv("OPENAI_API_KEY")
+        if api_key:
+            params['api_key'] = api_key
+        else:
+            raise ValueError("No backend config found. Set BASE_URL and API_KEY or OPENROUTER_API_KEY or OPENAI_API_KEY environment variables")
+
+client_sync = OpenAI(**params)
+client_async = AsyncOpenAI(**params)
+
+def _chat(client, model, roles, messages, structure, tokens):
+    messages = [{'role': role, 'content': content} for role, content in zip(roles, messages)]
+
+    if structure == str:
+        return (client.chat.completions.create(model=model, messages=messages, max_completion_tokens=tokens), 
+                lambda response: response.choices[0].message.content)
+    else:
+        if issubclass(structure, BaseModel):
+            return (client.beta.chat.completions.parse(
+                model=model,
+                messages=messages,
+                response_format=structure,
+                max_completion_tokens=tokens
+            ), lambda response: response.choices[0].message.parsed)
+        else:
+            # OpenAI interface supports pydantic models only
+            # But we can just wrap anything into a pydantic model and unwrap it back
+            # This will be our little secret
+            return (client.beta.chat.completions.parse(
+                model=model,
+                messages=messages,
+                response_format=create_model('ModelResponse', response=structure),
+                max_completion_tokens=tokens
+            ), lambda response: response.choices[0].message.parsed.response)  
+
+async def write(model=DEFAULT_MODEL, roles=['system', 'user'], messages=[], structure=str, tokens=DEFAULT_TOKENS):
+    response, postproc = _chat(client_async, model, roles=roles, messages=messages, structure=structure, tokens=tokens)
+    return postproc(await response)
+
+def talk(model=DEFAULT_MODEL, roles=['system', 'user'], messages=[], structure=str, tokens=DEFAULT_TOKENS):
+    response, postproc = _chat(client_sync, model, roles=roles, messages=messages, structure=structure, tokens=tokens)
+    return postproc(response)
+
+def vibe(model=DEFAULT_MODEL, tokens=DEFAULT_TOKENS):
+    def _vibe(f):
+        rt = f.__annotations__.get('return', str)
+        if asyncio.iscoroutinefunction(f):
+            @wraps(f)
+            async def __vibe(*args, **kwargs):
+                return await write(model, messages=[f.__doc__, await f(*args, **kwargs)], 
+                                   structure=rt, tokens=tokens)
+        else:
+            @wraps(f)
+            def __vibe(*args, **kwargs):
+                return talk(model, messages=[f.__doc__, f(*args, **kwargs)], structure=rt, tokens=tokens)
+        return __vibe
+    return _vibe
