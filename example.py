d21797e8bb3d8d0a86beaec39c3d4b984994c65b
Vadim Liventsev <dev@vadim.me>
2025-08-27 17:15:50 +0200

    example fix

--- example.py
+++ example.py
@@ -24,11 +24,11 @@ async def is_alive(slug):
         return False
 
 async def openrouter_models():
-    models = requests.get("https://openrouter.ai/api/frontend/models/find?order=top-weekly").json()['data']['models']
-    models = ((model, is_general_purpose(model['description'])) for model in models)
+    models = requests.get("https://openrouter.ai/api/frontend/models/find?order=top-weekly").json()['data']['models'][:10]
+    models = [(model, is_general_purpose(model['description'])) for model in models]
     models = (model for model, general_purpose in models if await general_purpose)
-    models = ((model, is_alive(model['slug'])) async for model in models)
-    models = (model async for model, alive in models if await alive)
+    models = [(model, is_alive(model['slug'])) async for model in models]
+    models = (model for model, alive in models if await alive)
 
     async for model in models:
         print(model['slug'])
